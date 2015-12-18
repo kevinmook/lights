1ec959cbf1141293ee6588fb25b658642df6ef4a
Kevin Mook <kevin@kevinmook.com>
2015-12-18 01:06:42 -0500

    toggle the lights with a GET request to /

--- arduino/christmas.c/christmas.c.ino
+++ arduino/christmas.c/christmas.c.ino
@@ -2,6 +2,7 @@
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 
 #include <Adafruit_NeoPixel.h>
+#include <DigiFi.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
@@ -11,37 +12,97 @@
 #define PIN            6
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      16
+#define NUMPIXELS      60
+
+#define SERVER_PORT    8080
+
+#define BUFSIZE 255
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-int delayval = 500; // delay for half a second
+DigiFi server;
+bool pixelsChanged = true;
+bool currentLineIsBlank = false;
+int red = 0;
+int green = 150;
+int blue = 0;
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  // End of trinket special code
+  setupPixels();
+  startServer();
+}
 
+void setupPixels() {
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
+void startServer() {
+  // start the server:
+  server.begin();
+  server.server(SERVER_PORT); //start server on port 8080
+  while (server.ready() != 1) {
+    delay(100);
+  }
+}
+
 void loop() {
+  if (server.serverRequest()) {
+    handleServerLoop();
+  } else if(pixelsChanged) {
+    setColors();
+    sendColors();
+  }
 
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+  delay(1);
+}
 
+void handleServerLoop() {
+  if(server.serverRequestPath() == "/") {
+    // send a standard http response header
+    server.println("HTTP/1.1 200 OK");
+    server.println("Content-Type: text/html");
+    server.println("Connection: close");  // the connection will be closed after completion of the response
+    //server.println("Refresh: 5");  // refresh the page automatically every 5 sec
+    server.println("Transfer-Encoding: chunked"); 
+    server.println();
+    server.printChunk("<!DOCTYPE HTML>");
+    server.printChunk("<html>");
+    server.printChunk("<pre>");
+    // server.printChunk(clientLine);
+    server.printChunk("</pre>");
+    server.printChunk("</html>");
+    server.closeChunk();
+
+    updateColors();
+  } else {
+    server.serverResponse("404 Not Found", 404);
+    delay(10);
+  }
+}
+
+void updateColors() {
+  if(green == 150) {
+    green = 0;
+    red = 150;
+  } else {
+    green = 150;
+    red = 0;
+  }
+
+  pixelsChanged = true;
+}
+
+void setColors() {
   for(int i=0;i<NUMPIXELS;i++){
 
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
-
-    pixels.show(); // This sends the updated pixel color to the hardware.
-
-    delay(delayval); // Delay for a period of time (in milliseconds).
-
+    pixels.setPixelColor(i, pixels.Color(red, green, blue)); // Moderately bright green color.
   }
+}
+
+void sendColors() {
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  pixelsChanged = false;
 }
